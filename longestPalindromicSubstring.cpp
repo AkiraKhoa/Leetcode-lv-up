d179f93df840fb059b2e58fb4ef445d9bf6f3c8f
AkiraKhoa <125184970+AkiraKhoa@users.noreply.github.com>
2023-08-26 15:43:56 +0000

    edit Longest Palindromic Substring gud runtime bad memory

--- longestPalindromicSubstring.cpp
+++ longestPalindromicSubstring.cpp
@@ -6,35 +6,54 @@ public:
             return s;
         }
 
-        int start = 0;
-        int maxLength = 1;
+        string t = preProcess(s);
+        n = t.length();
+        vector<int> p(n, 0);
+        int center = 0, right = 0;
+        int maxLen = 0, maxCenter = 0;
 
-        for (int i = 0; i < n; ++i) {
-            int left = i - 1;
-            int right = i + 1;
+        for (int i = 1; i < n - 1; ++i) {
+            int mirror = 2 * center - i;
 
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLength) {
-                    start = left;
-                    maxLength = right - left + 1;
-                }
-                left--;
-                right++;
+            if (i < right) {
+                p[i] = min(right - i, p[mirror]);
             }
 
-            left = i;
-            right = i + 1;
+            // Attempt to expand palindrome centered at i
+            while (t[i + p[i] + 1] == t[i - p[i] - 1]) {
+                ++p[i];
+            }
 
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLength) {
-                    start = left;
-                    maxLength = right - left + 1;
-                }
-                left--;
-                right++;
+            // If palindrome centered at i expands past right,
+            // adjust center based on palindrome's new right edge
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+
+            // Update the longest palindrome if necessary
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                maxCenter = i;
             }
         }
 
-        return s.substr(start, maxLength);
+        int start = (maxCenter - maxLen) / 2;
+        return s.substr(start, maxLen);
+    }
+
+private:
+    string preProcess(const string& s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
+        }
+
+        string t = "^";
+        for (int i = 0; i < n; ++i) {
+            t += "#" + s.substr(i, 1);
+        }
+        t += "#$";
+        return t;
     }
 };
